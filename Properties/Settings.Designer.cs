ac1876ec8dff9dc3d146d6cac18eb0798886b03d
kekewicho <luisruben2997@gmail.com>
2024-04-25 23:39:38 -0600

    Se modifican propiedades de tabla en Home y se agrega btnBuscar en View VentaTickets_SelectViaje

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace ReserBus.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=NeonByte;Initial Catalog=ReserBus;Integrated Security=True;Encrypt=Tr" +
-            "ue;TrustServerCertificate=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PCLUIS\\SQLEXPRESS;Initial Catalog=ReserBus;Integrated Security=True;E" +
+            "ncrypt=True;TrustServerCertificate=True")]
         public string ReserBusConnectionString {
             get {
                 return ((string)(this["ReserBusConnectionString"]));
